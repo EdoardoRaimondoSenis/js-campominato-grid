be611f4d7c8a17870e0497b8188a11c552b8db42
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-13 17:54:48 +0200

    griglia javascript con box che si colora al click

--- js/myscript.js
+++ js/myscript.js
@@ -1,2 +1,16 @@
+let container = document.getElementById("prisultato")
 
+for (let i = 0; i < 64; i++) {
+    let quadro = document.createElement("div");
+    quadro.classList.add("risultato");
+
+    quadro.addEventListener("click",
+        function () {
+            this.classList.toggle("sfondoclick");
+        }
+    );
+    console.log(quadro);
+    container.appendChild(quadro);
+
+}
 
